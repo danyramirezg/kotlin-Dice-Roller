7b1e25532103367218928c4ffe83f37e276a076b
danyramirezg <danyramirezg@gmail.com>
2021-05-27 10:35:25 -0500

    Finding views efficiently -using lateinit var

--- app/src/main/java/com/dany/diceroller/MainActivity.kt
+++ app/src/main/java/com/dany/diceroller/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,12 +22,13 @@ class MainActivity : AppCompatActivity() {
             // Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
 
         val randomInt = Random.nextInt(6) + 1
-        val drawableResource = when(randomInt){
+        val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -32,10 +36,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView =  findViewById(R.id.dice_image)
-
         // Assign the drawable resource:
         diceImage.setImageResource(drawableResource)
     }
-
 }
